9d7ce30b08b9b26171cddcb78cada08af06416f5
congxia wu <1398173038@qq.com>
2023-11-10 09:54:02 +0800

    v0.0.0

---
+++ A_print.go
@@ -0,0 +1,13 @@
+package A_print
+
+import "fmt"
+import PrintNow "github.com/deer7777/B"
+
+/*func main() {
+	A_print()
+}*/
+
+func A_print() {
+	fmt.Println("version A_v0.0.0")
+	PrintNow.PrintNow()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/deer7777/A_gomodule
+
+go 1.21.1
+
+require github.com/deer7777/B v1.0.0
